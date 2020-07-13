2cb5c85c093b4c7b6d7b156c0fd5fab8af1c1f84
Anna Romashenko <aa.romashchenko@gmail.com>
2020-07-13 17:33:41 +0300

    Updates

---
+++ precache-manifest.c0e3fe7e0ad6214b2375e7f908374911.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292b0131047f20eeeda211e2fe1b834c",
+    "url": "/the-star-wars-react/index.html"
+  },
+  {
+    "revision": "c53930bca9a2cf190894",
+    "url": "/the-star-wars-react/static/css/main.93ec92a8.chunk.css"
+  },
+  {
+    "revision": "958ea2842de2701aeed0",
+    "url": "/the-star-wars-react/static/js/2.c585a979.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/the-star-wars-react/static/js/2.c585a979.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c53930bca9a2cf190894",
+    "url": "/the-star-wars-react/static/js/main.f3bc8331.chunk.js"
+  },
+  {
+    "revision": "1487a8e7b5478aed6793",
+    "url": "/the-star-wars-react/static/js/runtime-main.269262fa.js"
+  },
+  {
+    "revision": "1502fac93eeeacad24042522e0bd5ca2",
+    "url": "/the-star-wars-react/static/media/poller-one-v9-latin-regular.1502fac9.woff2"
+  },
+  {
+    "revision": "17a464e9a06c3f278b790ba7cc37e287",
+    "url": "/the-star-wars-react/static/media/poller-one-v9-latin-regular.17a464e9.eot"
+  },
+  {
+    "revision": "5f381ce46d37be0ce2f6253a64b1eea3",
+    "url": "/the-star-wars-react/static/media/poller-one-v9-latin-regular.5f381ce4.ttf"
+  },
+  {
+    "revision": "f3d7496346d28060e819ab28648cd638",
+    "url": "/the-star-wars-react/static/media/poller-one-v9-latin-regular.f3d74963.svg"
+  },
+  {
+    "revision": "f92d1fc8af43244147164425ab6b3826",
+    "url": "/the-star-wars-react/static/media/poller-one-v9-latin-regular.f92d1fc8.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/the-star-wars-react/precache-manifest.c0e3fe7e0ad6214b2375e7f908374911.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/the-star-wars-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
